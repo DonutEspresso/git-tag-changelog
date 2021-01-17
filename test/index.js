605ce45f14c46331b1c6c383cce9e072903995a9
Alex Liu <DonutEspresso@users.noreply.github.com>
2021-01-17 01:24:02 -0800

    chore(deps): upgrade dependencies (#4)

--- test/index.js
+++ test/index.js
@@ -1,11 +1,13 @@
 'use strict';
 
 // external modules
+const assert = require('chai').assert;
 const execSync = require('child_process').execSync;
 
-describe('git-tag-changelog', function() {
-    it('should be awesome', function(cb) {
-        execSync('bin/cli.js -h');
+describe('git-tag-changelog', function () {
+    it('should run cli and show help message', function (cb) {
+        const output = execSync('bin/cli.js -h').toString();
+        assert.include(output, 'usage: cl [ACTION] [OPTIONS]');
         return cb();
     });
 });
